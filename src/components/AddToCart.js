97c6d6c62220ee57fb8c6ae4c6376bf018641f8c
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-02-11 22:21:22 +0000

    addtocart context setup

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -43,7 +43,7 @@ const AddToCart = ({product}) => {
     </div>
     <div className="btn-container">
       <AmountButtons amount={amount} increase={increase} decrease={decrease} />
-      <Link to='/cart' className='btn' >
+      <Link to='/cart' className='btn' onClick={() => addToCart(id,mainColor,amount,product)}>
         add to cart
       </Link>
     </div>
